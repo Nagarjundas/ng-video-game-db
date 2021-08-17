0952ae140bf71796d146927b41328f852a749658
NAGARJUN <dasnagarjun1994@gmail.com>
2021-08-17 19:47:07 +0530

    c2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{HttpClientModule} from '@angular/common/http';
+import { GaugeModule } from 'angular-gauge';
+
+
+import{MatTabsModule} from '@angular/material/tabs';
+import{MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { SearchBarComponent } from './Components/search-bar/search-bar.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule,FormsModule,HttpClientModule,GaugeModule.forRoot(),MatTabsModule,
+    MatIconModule,MatFormFieldModule,MatSelectModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
